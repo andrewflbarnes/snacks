1fc0445a753b6d0b623d47e621006c4ee32dfe0d
andrewflbarnes <andrew.barnes2@mastercard.com>
2020-08-21 19:04:37 +0100

    Add ability to construct and send basic HTTP POST request
    - Generate POST request manually as a string so we can later send
      groups of bytes down a raw TCP connection rather than leveraging
      an HTTP framework which likely wouldn't allow this

---
+++ loris/loris.go
@@ -0,0 +1,50 @@
+package loris
+
+import (
+	"text/template"
+	"bytes"
+)
+
+var (
+	httpTmpl = `POST {{.Endpoint}} HTTP/1.1
+Content-Length: {{.Length}}
+Content-Type: {{.ContentType}}
+User-Agent: snacks
+
+{{.Body}}
+
+
+`
+)
+
+type Loris struct {
+	Tmpl *template.Template
+}
+
+type LorisVals struct {
+	Endpoint string
+	Length int
+	ContentType string
+	Body string
+}
+
+func (l Loris) Build(vals LorisVals) string {
+	var resolved bytes.Buffer
+	if err := l.Tmpl.Execute(&resolved, vals); err != nil {
+		panic(err)
+	}
+
+	return resolved.String();
+}
+
+func New() Loris {
+	tmpl, err := template.New("default").Parse(httpTmpl)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return Loris {
+		Tmpl: tmpl,
+	}
+}
--- main.go
+++ main.go
@@ -4,20 +4,72 @@ import (
     "fmt"
     "net"
     "bufio"
+    "andrewflbarnes/snacks/loris"
+    "strconv"
+)
+
+var (
+    port = 8989
 )
 
 func main() {
-    ln, err := net.Listen("tcp", ":8111")
+    body := `{"ab":"cd"}`
+
+    l := loris.New();
+    lParams := loris.LorisVals{
+        Endpoint: "/",
+        ContentType: "application/json",
+        Length: len(body),
+        Body: body,
+    }
+
+    payload := l.Build(lParams)
+
+    serverReady := make(chan bool)
+
+    go server(port, serverReady)
+
+    <-serverReady
+
+    sendPayload("localhost", port, payload)
+}
+
+func sendPayload(host string, port int, payload string) {
+    dest := host + ":" + strconv.Itoa(port)
+    fmt.Println("CLIENT: Opening connection to " + dest)
+
+    conn, err := net.Dial("tcp", dest)
+    if err != nil {
+        panic(err)
+    }
+    defer conn.Close()
+
+    fmt.Println("CLIENT: Sending payload to " + dest)
+    fmt.Fprintf(conn, payload)
+    status, err := bufio.NewReader(conn).ReadString('\n');
+    if  err != nil {
+        panic(err)
+    }
+
+    fmt.Println("CLIENT: Received response:\n" + status)
+}
+
+func server(port int, ready chan bool) {
+    strPort := strconv.Itoa(port)
+    fmt.Println("SERVER: Opening server on: " + strPort)
+    ln, err := net.Listen("tcp", "127.0.0.1:" + strPort)
 
     if err != nil {
-        fmt.Println(err.Error())
-        return
+        panic(err)
     }
+
+    ready<-true
 
     for {
         conn, err := ln.Accept()
+        fmt.Println("SERVER: Accepted connection on " + strPort)
         if err != nil {
-            fmt.Println(err.Error())
+            panic(err)
         } else {
             go handleConnection(conn)
         }
@@ -25,18 +77,31 @@ func main() {
 }
 
 func handleConnection(conn net.Conn) {
+    lastBlank := false
+    fullMsg := ""
+
+    defer conn.Close()
+
+    reader := bufio.NewReader(conn)
     for {
-        msg, err := bufio.NewReader(conn).ReadString('\n')
+        msg, err := reader.ReadString('\n')
         if err != nil {
-            fmt.Println(err.Error())
-            fmt.Printf("Closing connection: %q\n", conn)
-            conn.Close()
-            return
-        } else {
-            fmt.Println("PING")
-            fmt.Println(msg)
-            fmt.Fprintf(conn, msg)
-            fmt.Println("PONG")
+            panic(err)
+        }
+        
+        fullMsg += msg
+        if msg == "\n" {
+            if lastBlank {
+                fmt.Println("SERVER: Received payload:\n" + fullMsg)
+                fullMsg = ""
+                lastBlank = false
+
+                response := "Received\n"
+                fmt.Print("SERVER: Sending response: " + response)
+                fmt.Fprintf(conn, response)
+            } else {
+                lastBlank = true
+            }
         }
     }
 }
